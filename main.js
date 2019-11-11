fec26f0aa4d40d7b4b20c86c985e728c0e61d4c5
michele <michele.provolo@gmail.com>
2019-11-11 08:10:05 +0100

    bozza navabar

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-$( document ).ready(function() {
 
-});
+$(".dropdown > a").click(function() {
+
+    if (!$(this).hasClass("active")) {
+      $(".parent li").removeClass("active");
+      $(".dropdowncontent").removeClass("open");
+      $(this).addClass("active");
+      $(".dropdowncontent").addClass("open");
+      
+    } else {
+      $(".parent li").removeClass("active");
+      $(".dropdowncontent").removeClass("open");
+    
+    }
+  });
